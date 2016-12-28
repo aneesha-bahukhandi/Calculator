c875a32b4d187f5e420fc5aca8f926a9207060c4
aneesha-bahukhandi <aneesha.bahukhandi@gmail.com>
2016-12-28 17:31:46 +0530

    added more files

--- android/app/src/main/java/com/calculator/MainApplication.java
+++ android/app/src/main/java/com/calculator/MainApplication.java
@@ -1,14 +1,13 @@
 package com.calculator;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
 
 import java.util.Arrays;
 import java.util.List;
